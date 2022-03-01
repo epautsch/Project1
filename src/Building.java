d5ca9960c774cbb6bad930dad8b5788617b9edc3
= <epautsch@luc.edu>
2022-02-28 23:17:08 -0600

    Building updated

--- src/Building.java
+++ src/Building.java
@@ -1,6 +1,34 @@
-public class Building {
+import java.util.ArrayList;
+
+public class Building implements  FacilityUse{
     public String name;
     public int numFloors;
     public int roomsPerFloor;
-    public
+    public int maxPatientCapacity;
+    public ArrayList<Room> currentVacancies;
+
+    public boolean isInUseDuringInterval() {
+        return false;
+    }
+
+    public Room assignFacilityToUse(String facility) {
+        return new Room();
+    }
+
+    public Room vacateFacility(String facility) {
+        return new Room();
+    }
+
+    public ArrayList<Inspection> listInspections() {
+        return new ArrayList<Inspection>();
+    }
+
+    public void listActualUsage() {
+
+    }
+
+    public float calcUsageRate() {
+        return 0.0f;
+    }
+
 }
